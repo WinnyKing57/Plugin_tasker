fb38d04bb9f61d1cc0cc54746e4d1d768d96fcb6
WinnyKing57 <130479819+WinnyKing57@users.noreply.github.com>
2025-06-20 19:35:54 +0200

    Delete taskerpluginlibrary directory

--- taskerpluginlibrary/src/main/java/com/joaomgcd/taskerpluginlibrary/output/TaskerPluginOutputForConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.joaomgcd.taskerpluginlibrary.output
-
-import android.content.Context
-import android.os.Bundle
-
-
-interface TaskerPluginOutputForConfig<TOutput : Any> : TaskerPluginOutput<TOutput> {
-    fun addOutputToBundle(context: Context, bundle: Bundle)
-}
--- taskerpluginlibrary/src/main/java/com/joaomgcd/taskerpluginlibrary/output/runner/TaskerPluginOutputValueGetter.kt
+++
@@ -1,8 +0,0 @@
-package com.joaomgcd.taskerpluginlibrary.output.runner
-
-import com.joaomgcd.taskerpluginlibrary.output.TaskerOutputVariable
-import kotlin.reflect.KProperty
-
-class TaskerOutputVariableValueGetter<T>(private val variable: TaskerOutputVariable, private val valueProvider: () -> T?) {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): T? = valueProvider()
-}
